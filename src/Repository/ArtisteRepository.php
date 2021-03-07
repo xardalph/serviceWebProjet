c9597735d0b76aca8e4301d252e33c4f0ddfcf6b
QuentinEPSI <quentin.georghiou@epsi.fr>
2021-03-07 19:54:52 +0100

    Affichage artiste

--- src/Repository/ArtisteRepository.php
+++ src/Repository/ArtisteRepository.php
@@ -18,33 +18,4 @@ class ArtisteRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Artiste::class);
     }
-
-    // /**
-    //  * @return Artiste[] Returns an array of Artiste objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Artiste
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
