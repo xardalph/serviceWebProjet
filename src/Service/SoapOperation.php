e0f118c8ee9fa8a3bc1687b0ec857e0f92705c2b
evan <evan.adam@yahoo.fr>
2021-03-07 21:32:13 +0100

    Final

--- src/Service/SoapOperation.php
+++ src/Service/SoapOperation.php
@@ -22,62 +22,14 @@ class SoapOperations
     {
         $this->doct = $doct;
     }
-
     /**
-     * Dis "Hello" à la personne passée en paramètre
-     * @param string $name Le nom de la personne à qui dire "Hello!"
-     * @return string The hello string
-     */
-    public function sayHello(string $name): string
-    {
-        return 'Hello ' . $name . '!';
-    }
-
-    /**
-     * Réalise la somme de deux entiers
-     * @param int $a 1er nombre
-     * @param int $b 2ème nombre
-     * @return int La somme des deux entiers
-     */
-    public function sumHello(int $a, int $b): int
-    {
-        return (int)($a + $b);
-    }
-
-    /**
-     * @param float $a
-     * @param float $b
-     * @param float $c
-     * @return float
-     */
-    public function sumFloats(float $a, float $b, float $c): float
-    {
-        return (float)($a + $b + $c);
-    }
-
-
-    /**
-     * Récupère le libellé d'un secteur dont on connaît l'id
-     * @return \App\Soap\MorceauSoap Le Morceau qu'on veux
+     * Récupère morceau Depuis la base de données avec son id
+     * @return \App\Soap\MorceauSoap Le morceau
      */
     public function getMorceauLibelle($mor)
     {
 
         $mor2 = $this->doct->getRepository(\App\Entity\Morceau::class)->find($mor);
-        $morceau = new MorceauSoap($mor2->getId(), $mor2->getTitre(), $mor2->getDuree() );
-
-        file_put_contents("./tests2", serialize($morceau->titre));
-
-        return $morceau;
-    }
-    /**
-     * Récupère le libellé d'un secteur dont on connaît l'id
-     * @return \App\Soap\MorceauSoap Le Morceau qu'on veux
-     */
-    public function getGroupe($id)
-    {
-
-        $mor2 = $this->doct->getRepository(\App\Entity\Morceau::class)->find($id);
         $morceau = new MorceauSoap($mor2->getId(), $mor2->getTitre(), $mor2->getDuree() );
 
         file_put_contents("./tests2", serialize($morceau->titre));
