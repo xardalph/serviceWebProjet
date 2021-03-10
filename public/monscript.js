9ea4258b05bd31fcf4e3d6f3975afc9629f77579
evan <evan.adam@yahoo.fr>
2021-03-10 21:34:32 +0100

    le script affiche les données correctement

--- public/monscript.js
+++ public/monscript.js
@@ -1,19 +1,29 @@
 
 
-var request = new XMLHttpRequest();
+const userAction = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/artistes');
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log(myJson);
+    document.getElementById('json').textContent = JSON.stringify(myJson, undefined, 2);
+}
+
+userAction();
 
 
-request.open("GET", "http://127.0.0.1:8000/api/artistes/1", false);
+var data = {
+    "data": {
+        "x": "1",
+        "y": "1",
+        "url": "http://url.com"
+    },
+    "event": "start",
+    "show": 1,
+    "id": 54485485
+}
 
-request.send(null);
 
-
-console.log(request.responseText);
-
-
-
-
-document.getElementById('json').textContent = JSON.stringify(request.responseText, undefined, 2);
+//document.getElementById('json').textContent = JSON.stringify(request.responseText, undefined, 2);
 
 
 
